4202f2556f2a240dfce4fc598942bf0ab98c8e08
Yoni190 <yfisuhu@gmail.com>
2025-03-04 13:35:05 +0300

    Add status to class 'List'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-class todo {
-    constructor(title, description, dueDate, priority){
+class List {
+    constructor(title, description, dueDate, priority, status){
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.status = stauts;
     }
 
     display() {
-        console.log(`Title: ${this.title}, Description: ${this.description}, Due Date: ${this.dueDate}, Priority: ${this.priority}`);
+        console.log(`Title: ${this.title}, Description: ${this.description}, Due Date: ${this.dueDate}, Priority: ${this.priority}, Status: ${this.status}`);
     }
 }
